872b5c0b8f909f0b4214678cfe6a3b67d4269ce1
Plezo <Plezo@protonmail.com>
2023-01-01 00:53:56 -0500

    Solved first problem

---
+++ 1_2022.py
@@ -0,0 +1,20 @@
+
+lines = []
+
+inp = open('input.txt', 'r')
+lines = inp.readlines()
+inp.close()
+
+res = []
+
+rolling_sum = 0
+for line in lines:
+    if line != '\n':
+        num = int(line[0:-1])
+        rolling_sum += num
+    else:
+        res.append(rolling_sum)
+        rolling_sum = 0
+
+res.sort()
+print(sum(res[-3:]))
